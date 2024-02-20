ff54af71e8dfc10028dfd5ff09d47bc403ae680b
Michel Novus <michelnovus@gmail.com>
2024-02-20 04:10:50 -0300

    Some basic structures.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 // [MIT License] Copyright (c) 2024 Michel Novus
 
+#![allow(dead_code)]
+
+use std::env;
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+
 fn main() {}
+
+/// Creates the program configuration file.
+fn generate_config_file(path: PathBuf) -> io::Result<()> {
+    Ok(())
+}
+
+/// Contains the initial configuration of the program.
+struct AppConfig {}
+
+impl AppConfig {
+    /// Generates a new `AppConfig` struct with defaults attributes.
+    fn new() -> Self {
+        AppConfig {}
+    }
+
+    /// Tries to generates a new `AppConfig` struct from file configuration.
+    ///
+    /// The function return `Err` if file is not valid.
+    fn from_file(path: PathBuf) -> io::Result<Self> {
+        Ok(AppConfig {})
+    }
+}
